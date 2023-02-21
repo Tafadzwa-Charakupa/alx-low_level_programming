610136f6ba98ede089c390f1492ae4cc71f98abb
Tafadzwa-Charakupa <charakupatafah2@gmail.com>
2023-02-21 06:48:43 -0800

    Tasks

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print all alphabet in lower cases
+ */
+
+void print_alphabet(void)
+{
+	char lt;
+
+	for (lt = 'a'; lt <= 'z'; lt++)
+		_putchar(lt);
+
+	_putchar('\n');
+}
+
---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - Computes the absolute value of an integer
+ * @i: the integer to be computed
+ * Return: The absolute value
+ */
+int _abs(int i)
+{
+	if (i >= 0)
+		return (i);
+	else
+		return (-i);
+}
+
