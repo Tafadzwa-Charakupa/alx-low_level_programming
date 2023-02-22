10a43bc82330218e966fd423be069885c8a018fb
Tafadzwa-Charakupa <charakupatafah2@gmail.com>
2023-02-21 16:02:10 -0800

    Tasks

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio>
+
+/**
+ * main - main block
+ * desscription: computes and prints the sum of all the maltiples of 
+ * 5 below 1024 (excluded), followed by a new line
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 0;
+	long int a = 0, b = 1, next;
+
+	while (i < 50)
+	{
+		next = a + b;
+		a = b;
+		b = next;
+		printf("%lu", next);
+
+		if (i < 49)
+		{
+			printf(", ");
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
