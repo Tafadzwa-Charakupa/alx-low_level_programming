409c621dba9d7b709c8d8407531eab3aa611f78e
Tafadzwa-Charakupa <charakupatafah2@gmail.com>
2023-03-02 02:43:11 -0800

    Infinite add

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -70,7 +70,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	for (index = 0; *(n2 + index); index++)
 		n2_len++;
 
-	if (size_r <= n1_len + || size_r <= n2_len + 1)
+	if (size_r <= n1_len + 1 || size_r <= n2_len + 1)
 		return (0);
 
 	n1 += n1_len - 1;
