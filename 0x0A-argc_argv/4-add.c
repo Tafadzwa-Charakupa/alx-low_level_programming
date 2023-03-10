1f8ae86df323f8a843653e64c9d7ec9582e0bfd3
Tafadzwa-Charakupa <charakupatafah2@gmail.com>
2023-03-10 04:11:51 -0800

    Task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,18 +12,18 @@ int main(int argc, char *argv[])
 {
 	int result = 0, num, i, j;
 
-	if (argc == 1)
-		printf("0\n");
-
-	for (i = 0; i < argc; i++)
+	for (i = 1; i < argc; i++)
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			printf("%s\n", "Error");
-			return (1);
+			if (argv[i][j] > '9' || argv[1][j] < '0')
+			{
+				printf("%s\n", "Error");
+				return (1);
+			}
 		}
 	}
-	for (k = 0; k < argc; k++)
+	for (k = 1; k < argc; k++)
 	{
 		num = atoi(argv[k]);
 		result += num;
