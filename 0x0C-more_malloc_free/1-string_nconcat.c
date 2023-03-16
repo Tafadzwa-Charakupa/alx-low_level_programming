44029c19176184ed464db7ecfac330388cd4b73d
Tafadzwa-Charakupa <charakupatafah2@gmail.com>
2023-03-16 04:22:52 -0700

    Task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -22,23 +22,24 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	for (ls1 = 0; s1[ls1] != '\0'; ls1++)
 
-		for (ls2 = 0; s2[ls2] != '\0'; ls2++)
+	for (ls2 = 0; s2[ls2] != '\0'; ls2++)
 
-			if (n > ls2)
-				n = ls2;
+	if (n > ls2)
+		n = ls2;
 
-			lsout = ls1 + n;
+	lsout = ls1 + n;
 
-			sout = malloc(lsout + 1);
+	sout = malloc(lsout + 1);
 
-		if (sout == NULL)
-			return (NULL);
+	if (sout == NULL)
+		return (NULL);
 
 	for (i = 0; i < lsout; i++)
 		if (i < ls1)
 			sout[i] = s1[i];
 		else
 			sout[i] = s2[i - ls1];
+
 	sout[i] = '\0';
 
 	return (sout);
