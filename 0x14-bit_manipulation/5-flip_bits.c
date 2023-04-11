59f853e20b8c743e302311384cdc2c52bc29ef48
Tafadzwa-Charakupa <charakupatafah2@gmail.com>
2023-04-11 04:58:20 -0700

    Tasks'

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -5,7 +5,7 @@
  * @n: the number
  * @m: the number to flip n to
  *
- * Return: the necessary number of bits to flip to get fronm n to m
+ * Return: the number of bits
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
@@ -14,7 +14,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	while (xor > 0)
 	{
 		bits += (xor & 1);
-		xor >> 1;
+		xor >>= 1;
 	}
 	return (bits);
 }
