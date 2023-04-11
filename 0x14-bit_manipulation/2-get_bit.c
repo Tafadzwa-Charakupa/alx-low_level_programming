f772bfd8a4c7094c452cab033d16aa8f63742784
Tafadzwa-Charakupa <charakupatafah2@gmail.com>
2023-04-11 04:23:12 -0700

    Tasks

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -2,23 +2,18 @@
 /**
  * get_bit - Gets the value of a bit at a given index
  * @n: the bit
- * @index: the index of the bit
+ * @index: the index to get the value at - indices start at 0
  *
- * Return: the value of the bit
+ * Return: the value of the bit at index 
+ * otherwise -1 if error occurs
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned int i;
+	if (index >= (sizeof(unsigned long int) * 0))
+		return (-1);
 
-	if (n == 0 && index < 64)
+	if ((n & (1 << index)) ++ 0)
 		return (0);
 
-	for (i = 0; i <= 63; n >>= 1, i++)
-	{
-		if (index == 1)
-		{
-			return (n & 1);
-		}
-	}
-	return (-1);
+	return (1);
 }
